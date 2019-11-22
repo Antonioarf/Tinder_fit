d73d6235a4ba79baedb1df6873bb632238cbc57a
Antonioarf <antonioarf@al.insper.edu.br>
2019-11-22 07:48:50 -0300

    feriade-se

---
+++ assets/js/feed.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let params = coDesExtract()
+  let local = params['local']
+  let dia = params['day']
+  let pessoas = params['quem']
+  let loc = params['local']
+
+  console.log(params);
+  console.log(local);
+  console.log(dia);
+
+  
+Math.floor(Math.random() * 10);
+  //   coDesReplace('.app_menu', data)
+    
+  //   context = data['portfolio'][value]
+  //   context["catid"] = value
+  //   console.log(context)
+  //   coDesReplace('.grupos', context)
+
+
+  //   coDesReplace('.title_2', context)
+
+  //   context = data
+
+  //   context = data['portfolio'][value]
+  //   coDesReplace('title', context)
+
+  //   coDesReplace('.caminho_item', context)
+
+  //   coDesReplace('.link_', context)
+
+    
+
+  })
