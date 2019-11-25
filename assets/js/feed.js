9a10440f867db85ad2bbebbd6a8457888219b914
Antonioarf <antonioarf@al.insper.edu.br>
2019-11-25 08:10:01 -0300

    kytr

--- assets/js/feed.js
+++ assets/js/feed.js
@@ -1,35 +1,66 @@
 document.addEventListener('DOMContentLoaded', function() {
   let params = coDesExtract()
-  let local = params['local']
-  let dia = params['day']
+  let local = params['onde']
+  let dia = params['quando']
   let pessoas = params['quem']
-  let loc = params['local']
+  let loc = params['onde']
+  let hora = params['hora']
 
   console.log(params);
   console.log(local);
   console.log(dia);
-
+  console.log(typeof params['hora']);
   
 // Math.floor(Math.random() * 10);
-  //   coDesReplace('.app_menu', data)
-    
-  //   context = data['portfolio'][value]
-  //   context["catid"] = value
-  //   console.log(context)
-  //   coDesReplace('.grupos', context)
+// PREPARANDO O HORARIO
+  var sem_minutos = hora.slice(0, 2);
+  console.log(sem_minutos);
+  numero= parseInt(sem_minutos, 10)
+  console.log(numero);
 
 
-  //   coDesReplace('.title_2', context)
+function myFunction2(ide, texto, variavel) {
+  var str = document.getElementById(ide).innerHTML; 
+  var res = str.replace(texto,variavel);
+  document.getElementById(ide).innerHTML = res;
+}
+  myFunction2("quantos","x",Math.floor(Math.random() * 3))
+  myFunction2("onde","onde",local)
+  myFunction2("quadra","y",Math.floor(Math.random() * 8))
+  myFunction2("hora","hora",numero+1)
+//
+//
+  myFunction2("quantos1","x",Math.floor(Math.random() * 3))
+  myFunction2("onde1","onde",local)
+  myFunction2("quadra1","y",Math.floor(Math.random() * 8))
+  myFunction2("hora1","hora",numero+2)
+//
+//
+//
+//
+// Get the modal
+var modal = document.getElementById("myModal");
 
-  //   context = data
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-  //   context = data['portfolio'][value]
-  //   coDesReplace('title', context)
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-  //   coDesReplace('.caminho_item', context)
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
-  //   coDesReplace('.link_', context)
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
-    
-
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
   })
--- assets/js/montar.js
+++ assets/js/montar.js
@@ -1,55 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let params = coDesExtract()
-  let jogo = params['atv']
-  console.log(params);
-  console.log(jogo);
+ 	let params = coDesExtract()
+ 	let jogo = params['atv']
+ 	console.log(params);
+ 	console.log(jogo);
+ 	console.log("vamo la meu querido");
 
-coDesReplace('.nome_esporte', jogo)
+	coDesReplace('.top', params['atv'])
+	console.log(params['atv']);
 
+// // var inputEmail= document.getElementById("email");
+// // localStorage.setItem("email", inputEmail.value);
 
-// // document.addEventListener('DOMContentLoaded', function() {
-// //     var resps = [];
-// //         $(document).on("click", ":submit", function(e) {
-// //             var stuff = ($($email).val())
-// //             resps = [JSON.stringify(stuff)];
+	function myFunction() {
+  // Get the checkbox
+  var checkBox = document.getElementByname("quando");
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    console.log(checkBox)
+  } 
+}
+function myFunction2() {
+  var str = document.getElementById("demo").innerHTML; 
+  var res = str.replace("jogo", params['atv']);
+  document.getElementById("demo").innerHTML = res;
+}
+	myFunction2()
 
-// //             localStorage.setItem('EmailsStuff', JSON.stringify(resps));
-// //     });
-// // })
-
-// let dict = [];
-//         // example {id:1592304983049, title: 'Deadpool', year: 2015}
-//         console.warn('salve salve');
-//         const addMovie = (ev)=>{
-//             ev.preventDefault(); 
-//             console.warn('salve salveq'); //to stop the form submitting
-//             let escolhas = {
-//                 quando: document.getElementByname('quando').value,
-//                 quem: document.getElementByname('quem').value,
-//                 quando: document.getElementByname('quando').value,
-//                 hora: document.getElementByname('hora').value
-//             }
-//             dict.push(escolhas);
-//             document.forms[0].reset(); // to clear the form for the next entries
-//             //document.querySelector('form').reset();
-//             //for display purposes only
-//             console.warn('added' , {dict} );
-//             let pre = document.querySelector('#msg pre');
-//             pre.textContent = '\n' + JSON.stringify(dict, '\t', 2);
-//             //saving to localStorage
-//             localStorage.setItem('MyMovieList', JSON.stringify(dict) );
-//         }
-//         document.addEventListener('DOMContentLoaded', ()=>{
-//             document.getElementById('btn').addEventListener('click', addMovie);
-//         });
-
-
-
-// //         function objectifyForm(formArray) {//serialize data function
-
-// //   var returnArray = {};
-// //   for (var i = 0; i < formArray.length; i++){
-// //     returnArray[formArray[i]['name']] = formArray[i]['value'];
-// //   }
-// //   return returnArray;
-// // }
+})
